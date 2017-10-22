d598d09ea5364d4768f2e20751c8b6461304e5a7
carl-halliburton <carl.halliburton@paradise.net.nz>
2017-10-22 20:48:30 +1300

    added functionality to turn on and off light

--- app/src/main/java/nz/co/carlhalliburton/readingmagnifier/MainActivity.java
+++ app/src/main/java/nz/co/carlhalliburton/readingmagnifier/MainActivity.java
@@ -1,5 +1,7 @@
 package nz.co.carlhalliburton.readingmagnifier;
 
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Camera camera;
+    public static boolean lightStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        lightStatus = false; //false off, true on
+        camera = Camera.open();
     }
 
     @Override
@@ -31,13 +39,37 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        //int id = item.getItemId();
+        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        //if (id == R.id.action_settings) {
-            //return true;
-        //}
-
+        if (id == R.id.action_light) {
+            toggleFlash();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void toggleFlash() {
+        View v = findViewById(android.R.id.content);
+
+        if (lightStatus) //true
+            toggleLightOff(v);
+        else //false
+            toggleLightOn(v);
+    }
+
+    public void toggleLightOn(View v) {
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(parameters);
+        camera.startPreview();
+        lightStatus = true;
+    }
+
+    public void toggleLightOff(View v) {
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        camera.setParameters(parameters);
+        camera.stopPreview();
+        lightStatus = false;
     }
 }
